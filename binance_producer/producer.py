fa91b6b66e8a679b757548551d29f043a7dff493
dejoker95 <dejoker95@gmail.com>
2021-06-23 17:44:07 +0900

    feat(binance_producer): add column filters, symbols.txt

--- binance_producer/producer.py
+++ binance_producer/producer.py
@@ -13,38 +13,34 @@ client = session.client("kinesis")
 
 def main():
 
-    # get all symbols
-    uri = "https://api.binance.com/api/v1/exchangeInfo"
-    sym = requests.get(uri).json()
     sym_list = []
+    with open("symbols.txt", "r") as f:
+        sym_list = f.readlines()
 
-    for i in range(len(sym["symbols"])):
-        if sym["symbols"][i]["quoteAsset"] == "BTC":
-            sym_list.append(sym["symbols"][i]["symbol"])
-    print(sym_list)
-    print(len(sym_list))
+    for i in range(len(sym_list)):
+        sym_list[i] = sym_list[i].strip()
 
     # 코인 종류 한번에 지정하기
-    # streams = []
-    # for i in range(50):
-    #     streams.append(sym_list[i].lower() + "@kline_1m")
-    # print()
-    # print(streams)
-    # print()
+    streams = []
+    for i in range(50):
+        streams.append(sym_list[i].lower() + "@kline_1m")
+    print()
+    print(streams)
+    print()
 
     # 테스트용 10개짜리
-    streams = [
-        "ethbtc@kline_1m",
-        "dogebtc@kline_1m",
-        "adabtc@kline_1m",
-        "bnbbtc@kline_1m",
-        "xrpbtc@kline_1m",
-        "dotbtc@kline_1m",
-        "maticbtc@kline_1m",
-        "tfuelbtc@kline_1m",
-        "solbtc@kline_1m",
-        "linkbtc@kline_1m",
-    ]
+    # streams = [
+    #     "ethbtc@kline_1m",
+    #     "dogebtc@kline_1m",
+    #     "adabtc@kline_1m",
+    #     "bnbbtc@kline_1m",
+    #     "xrpbtc@kline_1m",
+    #     "dotbtc@kline_1m",
+    #     "maticbtc@kline_1m",
+    #     "tfuelbtc@kline_1m",
+    #     "solbtc@kline_1m",
+    #     "linkbtc@kline_1m",
+    # ]
 
     # 레코드 임시 저장소
     kinesis_records = []
@@ -57,16 +53,42 @@ def main():
         coin = stream.split("@")[0].upper()
         dic_current[coin] = ""
 
+    def format_json_keys(data):
+        new_format = {
+            "t": "opentime",
+            "T": "closetime",
+            "s": "symbol",
+            "o": "open_price",
+            "c": "close_price",
+            "h": "high_price",
+            "l": "low_price",
+            "v": "base_asset_volume",
+            "n": "num_trades",
+            "q": "quote_asset_volume",
+            "V": "tb_base_asset_volume",
+            "Q": "tb_quote_asset_volume",
+        }
+        want = ["t", "T", "s", "o", "c", "h", "l", "v", "n", "q", "V", "Q"]
+
+        data_filtered = dict()
+        for col in want:
+            data_filtered[col] = data[col]
+
+        new_data = dict(
+            (new_format[key], value) for (key, value) in data_filtered.items()
+        )
+        return new_data
+
     # 각 쓰레드에서 msg 들어왔을때 작동할 메서드
     def handle_socket_message(msg):
-        data = msg["data"]["k"]  # dict type, 새로 받아온 데이터
+        data = format_json_keys(msg["data"]["k"])  # dict type, 새로 받아온 데이터
         coin = msg["data"]["s"]  # str type, 지금 다루고 있는 symbol, uppercase
         prev = dic_current[coin]  # 이전 값 불러오기
         dic_current[coin] = data  # 새로운 값 넣기
 
         # 이전 값이 null이 아니고 새로운 값이랑 시작 시간이 다르면
         # 이전 값을 kinesis_records에 append
-        if (prev != "") and (prev["t"] != dic_current[coin]["t"]):
+        if (prev != "") and (prev["opentime"] != dic_current[coin]["opentime"]):
             data = json.dumps(data)
             kinesis_records.append({"Data": data, "PartitionKey": coin})
             # 레코드가 10개 모이면 배치 전송, 테스트가 10개라서 10개로 함
