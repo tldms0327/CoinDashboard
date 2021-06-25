fb45b7a812a3b1d68208a043f75f6622a936ecb9
dejoker95 <dejoker95@gmail.com>
2021-06-25 23:59:48 +0900

    feat(lambda): change aws credential

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,14 +1,14 @@
 import boto3
 from requests_aws4auth import AWS4Auth
-import datetime
 import requests
 import json
+from time import time
 
 
-region = "ap-northeast-2"
-service = "es"
-creds = boto3.Session().get_credentials()
-awsauth = AWS4Auth(creds.access_key, creds.secret_key, region, service)
+# region = "ap-northeast-2"
+# service = "es"
+# creds = boto3.Session().get_credentials()
+# awsauth = AWS4Auth(creds.access_key, creds.secret_key, region, service)
 
 client = boto3.client("s3", "ap-northeast-2")
 BUCKET_NAME = "coinboard"
@@ -21,10 +21,9 @@ def sendData(data, index):
     url = host + "/" + index + "/" + t
     # index 마다 데이터가 다르니 여기에 서 변형해서 전송까지
     data.pop("index", None)
-    now = datetime.datetime.now()
-    t = now.strftime("%Y-%m-%d %H:%M:%S")
-    data["datetime"] = t
-    r = requests.post(url, auth=awsauth, json=data, headers=headers)
+    unix = int(time())
+    data["datetime"] = unix
+    r = requests.post(url, json=data, headers=headers)
 
 
 def handler(event, context):
@@ -32,7 +31,7 @@ def handler(event, context):
         bucket = record["s3"]["bucket"]["name"]
         key = record["s3"]["object"]["key"]
 
-        obj = client.get_object(bucket, key)
+        obj = client.get_object(Bucket=bucket, Key=key)
         body = obj["Body"].read()
         lines = body.splitlines()
 
