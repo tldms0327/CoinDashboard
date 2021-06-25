491fba86a21c6d419ceefb9b00539a2799f86552
Sieun <tldms0327@yonsei.ac.kr>
2021-06-26 02:34:47 +0900

    fix(lambda): auth problem

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,17 +1,35 @@
 import boto3
-from requests_aws4auth import AWS4Auth
-import requests
+from aws4auth.requests_aws4auth import AWS4Auth
+from botocore.vendored import requests
 import json
 from time import time
 
+region = "ap-northeast-2"
+service = "es"
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
-# region = "ap-northeast-2"
-# service = "es"
-# creds = boto3.Session().get_credentials()
-# awsauth = AWS4Auth(creds.access_key, creds.secret_key, region, service)
 
-client = boto3.client("s3", "ap-northeast-2")
-BUCKET_NAME = "coinboard"
+def lambda_handler(event, context):
+    client = boto3.client("s3", "ap-northeast-2")
+
+    for record in event["Records"]:
+        bucket = record["s3"]["bucket"]["name"]
+        key = record["s3"]["object"]["key"]
+
+    obj = client.get_object(Bucket=bucket, Key=key)
+    body = obj["Body"].read()
+    lines = body.splitlines()
+
+    if len(lines) != 0:
+        for line in lines:
+            line = json.loads(line.decode())
+            if "index" in line:
+                sendData(line, line["index"])
+
+    return {
+        'status': 200
+    }
 
 
 def sendData(data, index):
@@ -21,21 +39,8 @@ def sendData(data, index):
     url = host + "/" + index + "/" + t
     # index 마다 데이터가 다르니 여기에 서 변형해서 전송까지
     data.pop("index", None)
-    unix = int(time())
+    unix = int(time()) * 1000
     data["datetime"] = unix
-    r = requests.post(url, json=data, headers=headers)
 
-
-def handler(event, context):
-    for record in event["Records"]:
-        bucket = record["s3"]["bucket"]["name"]
-        key = record["s3"]["object"]["key"]
-
-        obj = client.get_object(Bucket=bucket, Key=key)
-        body = obj["Body"].read()
-        lines = body.splitlines()
-
-        if len(lines) != 0:
-            for line in lines:
-                line = json.loads(line.decode())
-                sendData(line, line["index"].lower())
+    r = requests.post(url, json=data, headers=headers, auth=awsauth)
+    print("status: ", r.text)
